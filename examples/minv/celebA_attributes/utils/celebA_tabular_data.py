f7177c36418f4beaad197c50615a0d0803acada3
oscarwernqvist <oscar@wernqvist.eu>
2025-02-27 09:51:49 +0000

    added celebA_tabular_dataset class

--- examples/minv/celebA_attributes/utils/celebA_tabular_data.py
+++ examples/minv/celebA_attributes/utils/celebA_tabular_data.py
@@ -1,35 +1,60 @@
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-# Load the dataframes from pickle files
-private_df = pd.read_pickle('private_df.pkl')
-public_df = pd.read_pickle('public_df.pkl')
-
 class CelebATabularDataset(Dataset):
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
+    """Dataset class for the CelebA attributes dataset."""
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
 
     def __len__(self):
-        return len(self.dataframe)
+        return len(self.features)
 
     def __getitem__(self, idx):
-        sample = self.dataframe.iloc[idx]
-        # Extract features and labels from the sample
-        # Assuming 'identity' is the label and the rest are features
-        features = sample.drop('identity').values
-        label = sample['identity']
-        return torch.tensor(features, dtype=torch.float32), torch.tensor(label, dtype=torch.long)
+        return self.features.iloc[idx], self.labels.iloc[idx]
 
-# Create datasets
-private_dataset = CelebATabularDataset(private_df)
-public_dataset = CelebATabularDataset(public_df)
+    @classmethod
+    def from_celebA(cls, path):
+        # read data from plk file
+        df = pd.read_pickle(path)
+        features = df.drop('identity', axis=1)
+        labels = df['identity']
 
-# Create dataloaders
-private_dataloader = DataLoader(private_dataset, batch_size=32, shuffle=True)
-public_dataloader = DataLoader(public_dataset, batch_size=32, shuffle=True)
+        return cls(features, labels)
 
-# Example of iterating through the dataloader
-for features, labels in private_dataloader:
-    print(features, labels)
-    break
+def get_celebA_test_train_loader(train_config):
+    """Get the train and test dataloaders for the CelebA dataset."""
+    train_fraction = train_config["data"]["f_train"]
+    test_fraction = train_config["data"]["f_test"]
+    batch_size = train_config["train"]["batch_size"]
+    data_dir =  train_config["data"]["data_dir"] + "/private_df.pkl"
+
+    # Load the data
+    private_dataset = CelebATabularDataset.from_celebA(data_dir)
+
+    dataset_size = len(private_dataset)
+    train_size = int(train_fraction * dataset_size)
+    test_size = int(test_fraction * dataset_size)
+
+    # Split the data into train and test sets stratified by the labels
+    train_indices, test_indices = train_test_split(range(dataset_size), test_size=test_size, train_size=train_size, stratify=private_dataset.labels)
+
+    train_subset = torch.utils.data.Subset(private_dataset, train_indices)
+    test_subset = torch.utils.data.Subset(private_dataset, test_indices)
+
+    train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+def get_celebA_public_loader(train_config):
+    """Get the public CelebA dataset."""
+    batch_size = train_config["train"]["batch_size"]
+    data_dir =  train_config["data"]["data_dir"] + "/public_df.pkl"
+
+    # Load the data
+    public_dataset = CelebATabularDataset.from_celebA(data_dir)
+
+    return DataLoader(public_dataset, batch_size=batch_size, shuffle=False)
